35693007b0fd0c5b18c554e05a1701e12ce9a294
EduardJavaJunior <eduardaceman@gmail.com>
2025-03-03 21:11:29 +0200

    fixess

--- Casino-Online/src/ui/footerRouter.jsx
+++ Casino-Online/src/ui/footerRouter.jsx
@@ -7,14 +7,14 @@ const Footer = () => {
     <div className="w-full left-0 right-0 bg-gray-500 backdrop-blur-lg bottom-0 fixed py-3 shadow-lg">
       <div className="flex justify-center items-center space-x-20">
         
-        <Link to={"/"}>
+        <Link to={"/game"}>
         <div className="flex flex-col text-white items-center cursor-pointer hover:text-green-600 transition-colors duration-200">
           <Gamepad className="w-8 h-8" />
           <span className="text-sm mt-1">Игра</span>
         </div>
         </Link>
 
-        <Link to={"/profile"}>
+        <Link to={"/"}>
         <div className="flex flex-col text-white items-center cursor-pointer hover:text-blue-600 transition-colors duration-200">
           <User className="w-8 h-8" />
           <span className="text-sm mt-1">Профиль</span>
