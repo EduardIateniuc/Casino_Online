7aa434b3b846a012bbbc6aa943c9f4dd6343f542
EduardJavaJunior <eduardaceman@gmail.com>
2025-03-23 16:36:32 +0200

    problems fix

--- Casino-Online/src/main.jsx
+++ Casino-Online/src/main.jsx
@@ -1,23 +1,25 @@
 
 import './index.css'
 import App from './App.jsx';
-import Profile from './ui/components/UserProfile.jsx'
+import Profile from './ui/components/Profile.jsx'
 import ReactDOM from "react-dom/client";
 import HistoryPage from './ui/components/HistoryPage.jsx';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Incomes from "./ui/components/BalanceIncomes.jsx";
 import Rating from './ui/components/Rating.jsx';
+import MainPage from './ui/components/MainPage.jsx';
 
 
 export default function Main() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Profile />}/>
+        <Route path="/" element={< MainPage/>}/>
         <Route path='/game' element={<App/>}/>
         <Route path='/history' element={<HistoryPage/>} />
-        <Route path='/balance-income' element={< Incomes/>}></Route>
-        <Route path='/rating' element={< Rating/>}></Route>
+        <Route path='/balance-income' element={< Incomes/>}/>
+        <Route path='/rating' element={< Rating/>}/>
+        <Route path='/profile' element={<Profile/>}/>
       </Routes>
     </BrowserRouter>
   );
